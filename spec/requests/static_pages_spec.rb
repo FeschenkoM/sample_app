97df6d23d3592ff5da2a1c1f284540840165fc60
FeschenkoM <mac9@inbox.ru>
2018-08-08 17:51:13 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,7 +16,7 @@ describe "StaticPages" do
     before { visit help_path }
 
     it {should have_content('Help')}
-    it {should have_title('Help')}
+    it {should have_title(full_title('Help'))}
 
   end
 
@@ -24,7 +24,7 @@ describe "StaticPages" do
     before { visit about_path }
 
         it {should have_content('About')}
-        it {should have_title('About')}
+        it {should have_title(full_title('About'))}
 
   end
 
@@ -32,6 +32,6 @@ describe "StaticPages" do
     before { visit contact_path }
 
     it {should have_content('Contact')}
-    it {should have_title('Contact')}
+    it {should have_title(full_title('Contact'))}
   end
 end
